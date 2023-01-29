8c769f6c260a6d0f8c833cc8fd7b37549445e666
HessahSWE <hissahalhussain@gmail.com>
2023-01-30 01:08:58 +0300

    update

---
+++ appointmentsfrontend/lib/components/loginForm.dart
@@ -0,0 +1,76 @@
+import 'package:appointmentsfrontend/components/button.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/config.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginForm();
+}
+
+class _LoginForm extends State<LoginForm> {
+  final _formKey = GlobalKey();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  bool obscurePass = true;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Config.primaryColor,
+            decoration: const InputDecoration(
+              hintText: 'Email Address',
+              labelText: 'Email',
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.email_outlined),
+              prefixIconColor: Config.primaryColor,
+            ),
+          ),
+          Config.spaceSmall,
+          TextFormField(
+            controller: _passController,
+            keyboardType: TextInputType.visiblePassword,
+            cursorColor: Config.primaryColor,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              labelText: 'Password',
+              alignLabelWithHint: true,
+              prefixIcon: const Icon(Icons.lock_outline),
+              prefixIconColor: Config.primaryColor,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    obscurePass = obscurePass;
+                  });
+                },
+                icon: obscurePass
+                    ? const Icon(
+                        Icons.visibility_off_outlined,
+                        color: Colors.black38,
+                      ) // Icon
+                    : const Icon(
+                        Icons.visibility_outlined,
+                        color: Config.primaryColor,
+                      ),
+              ),
+            ), // Icon // IconButton // Input Decoration
+          ),
+          Config.spaceSmall,
+          Button(
+              width: double.infinity,
+              title: 'Sign In',
+              onPressed: () {},
+              disable: false)
+        ],
+      ),
+    );
+  }
+}
---
+++ appointmentsfrontend/lib/utils/config.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static MediaQueryData? mediaQueryData;
+  static double? screenWidth;
+  static double? screenHight;
+
+  void init(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    screenWidth = mediaQueryData?.size.width;
+    screenHight = mediaQueryData?.size.height;
+  }
+
+  static get widthSize {
+    return screenWidth;
+  }
+
+  static get hightSize {
+    return screenHight;
+  }
+
+  static const spaceSmall = SizedBox(
+    height: 25,
+  );
+  static SizedBox spaceMedium = SizedBox(
+    height: screenHight ?? 0 * 0.05,
+  );
+  static SizedBox spaceBig = SizedBox(
+    height: screenHight ?? 0 * 0.08,
+  );
+  static const outlinedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  );
+  static const focusBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.greenAccent,
+      ));
+  static const errorBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.red,
+      ));
+  static const primaryColor = Colors.greenAccent;
+  static const greyColor = Color(0xFF616161);
+}
